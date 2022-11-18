41d8dd7dd315d6478a5535d360043e7ae1b5df5d
sarot60 <sarot.bu.60@ubu.ac.th>
2022-11-18 23:12:47 +0700

    updated main file - book service

--- book-service/src/main.ts
+++ book-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Http2RpcExceptionFilter } from './common/filters/http2rpc-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+    },
+  });
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalFilters(new Http2RpcExceptionFilter());
+
+  await app.listen()
 }
 bootstrap();
