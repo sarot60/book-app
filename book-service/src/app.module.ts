9461f2dc5b0702b48dd88ae6d039d0d31c005ba9
sarot60 <sarot.bu.60@ubu.ac.th>
2022-11-21 17:03:48 +0700

    updated app.module - book service

--- book-service/src/app.module.ts
+++ book-service/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { BookModule } from './book/book.module';
 import { MongooseConfigService } from './config/mongoose.config';
 
@@ -12,7 +11,5 @@ import { MongooseConfigService } from './config/mongoose.config';
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
