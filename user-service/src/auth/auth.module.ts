11cff17e34b722fe9b8369303306e1972c614b4e
sarot60 <sarot.bu.60@ubu.ac.th>
2022-11-21 16:52:29 +0700

    update auth module and update config mongoose - user service

--- user-service/src/auth/auth.module.ts
+++ user-service/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from '../user/user.module';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UserModule } from '../user/user.module';
 import { AuthHelper } from './helpers/auth.helper';
 import { LoginLimitHelper } from './helpers/login-limit.helper';
 
@@ -20,7 +21,7 @@ import { LoginLimitHelper } from './helpers/login-limit.helper';
         signOptions: { expiresIn: configService.get<string>('JWT_EXPIRE') },
       }),
       inject: [ConfigService],
-    })
+    }),
   ],
   providers: [AuthService, JwtStrategy, AuthHelper, LoginLimitHelper],
   controllers: [AuthController],
--- user-service/src/config/mongoose.config.ts
+++ user-service/src/config/mongoose.config.ts
@@ -4,20 +4,12 @@ import { MongooseModuleOptions, MongooseOptionsFactory } from "@nestjs/mongoose"
 
 @Injectable()
 export class MongooseConfigService implements MongooseOptionsFactory {
-
   @Inject(ConfigService)
   private readonly configService: ConfigService;
 
   createMongooseOptions(): MongooseModuleOptions {
-    const db: { user: string, password: string, host: string, name: string } = {
-      user: this.configService.get<string>('DATABASE_USER'),
-      password: this.configService.get<string>('DATABASE_PASSWORD'),
-      host: this.configService.get<string>('DATABASE_HOST'),
-      name: this.configService.get<string>('DATABASE_NAME'),
-    }
-    const mongodbUri = `mongodb+srv://${db.user}:${db.password}@${db.host}/${db.name}?retryWrites=true&w=majority`;
     return {
-      uri: mongodbUri
-    };
+      uri: this.configService.get<string>('DATABASE_URI')
+    }
   }
 }
