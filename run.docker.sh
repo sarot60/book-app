cf07e40c92a5ce361f4660869b3fb952da5a48b3
sarot60 <sarot.bu.60@ubu.ac.th>
2022-11-22 14:16:35 +0700

    update file shell script for run manual docker - root

--- run.docker.sh
+++ run.docker.sh
@@ -6,49 +6,52 @@ else
     echo "api_microservice network exists."
 fi
 
-# redis
-docker build -t nestjs-redis:book-app -f ./redis/Dockerfile .
+# mongodb
+docker build -t nestjs-mongodb:prod -f ./mongodb/Dockerfile .
+docker run -d \
+    -p 27017:27017 \
+    --restart always \
+    --net api_microservice \
+    --ip 172.20.100.10 \
+    --name nestjs-mongodb \
+    nestjs-mongodb:prod
 
+# redis
+docker build -t nestjs-redis:prod -f ./redis/Dockerfile .
 docker run -d \
     -p 6379:6379 \
     --restart always \
     --net api_microservice \
     --ip 172.20.100.3 \
     --name nestjs-redis \
-    nestjs-redis:book-app
-
+    nestjs-redis:prod
 
 # gateway
-docker build -t nestjs-gateway:book-app -f ./gateway/Dockerfile .
-
+docker build -t nestjs-gateway:prod -f ./gateway/Dockerfile .
 docker run -d \
     -p 3000:3000 \
     --restart always \
     --net api_microservice \
     --ip 172.20.100.2 \
     --name nestjs-gateway \
-    nestjs-gateway:book-app
-
+    nestjs-gateway:prod
 
 # user service
-docker build -t nestjs-user-service:book-app -f ./user-service/Dockerfile .
-
+docker build -t nestjs-user-service:prod -f ./user-service/Dockerfile .
 docker run -d \
     -p 4001:4001 \
     --restart always \
     --net api_microservice \
     --ip 172.20.100.4 \
     --name nestjs-user-service \
-    nestjs-user-service:book-app
-
+    nestjs-user-service:prod
 
 # book service
-docker build -t nestjs-book-service:book-app -f ./book-service/Dockerfile .
-
+docker build -t nestjs-book-service:prod -f ./book-service/Dockerfile .
 docker run -d \
     -p 4002:4002 \
     --restart always \
     --net api_microservice \
     --ip 172.20.100.5 \
     --name nestjs-book-service \
-    nestjs-book-service:book-app
+    nestjs-book-service:prod
